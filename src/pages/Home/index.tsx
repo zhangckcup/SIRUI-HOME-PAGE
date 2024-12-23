88aa20b404c18114269eb05c9196be4c676a2a2a
HZ-MB-0245 <hz-mb-0245@HZ-MB-0245deMacBook-Pro.local>
2024-12-23 14:28:26 +0800

    feat: updeta home img

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,17 +6,17 @@ import './index.less';
 export default function Home() {
   // banner图列
   const PcHomeImg = ['/t1.jpg', '/t2.jpg', '/t3.jpg', '/t4.jpg', '/t5.jpg'];
-  const MobileImg = ['/t1.jpg', '/t2.jpg', '/t3.jpg', '/t4.jpg', '/t5.jpg'];
+  const MobileImg = ['/mobile1.jpg', '/mobile2.jpg', '/mobile3.jpg', '/mobile4.jpg', '/mobile5.jpg'];
+  const RandomNum = Math.floor(Math.random() * PcHomeImg.length);
   // 随即透出其一
-  // const randomImg = PcHomeImg[Math.floor(Math.random() * PcHomeImg.length)];
   const randomImg = useMemo(() => {
-    if(getWidth()) {
-      return PcHomeImg[Math.floor(Math.random() * PcHomeImg.length)]
+    if (getWidth()) {
+      return PcHomeImg[RandomNum]
     } else {
-      return MobileImg[Math.floor(Math.random() * MobileImg.length)]
+      return MobileImg[RandomNum]
     }
   }, [getWidth()])
-  
+
 
   useEffect(() => {
     const scrollCb = () => {
@@ -28,14 +28,14 @@ export default function Home() {
       document.removeEventListener('wheel', scrollCb);
     };
   }, []);
+  console.log(RandomNum)
 
-  
   return (
-    <div className={`${getWidth() ? 'pc-all-wrap': 'mobile-all-wrap'} "home-wrap"`}>
+    <div className={`${getWidth() ? 'pc-all-wrap' : 'mobile-all-wrap'} "home-wrap"`}>
       <div className="pc-home-floor">
         {/* 背景图 */}
         <img className="pc-home-banner mobile-home-banner" alt={'Home Banner'} src={randomImg} />
-        <div className="pc-home-btn mobile-home-btn">
+        <div className={`pc-home-btn mobile-home-btn ${RandomNum > 2 ? 'mobile-center-btn' : ''}`}>
           <TryFreeBtn btnHref="https://test.sandwichlab.ai/lexi" />
         </div>
       </div>
